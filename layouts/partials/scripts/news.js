42452854dde463db6acd404c92a207f114789ed2
Matt Feldman <decaprime@gmail.com>
2015-12-31 19:54:44 -0800

    move to container from text container

--- layouts/partials/scripts/news.js
+++ layouts/partials/scripts/news.js
@@ -8,7 +8,7 @@ $(".rss-container").rss("http://steamcommunity.com/groups/bestarkserver/rss",
         // inner template for each entry
         // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
         // valid values: any string
-        entryTemplate: '<div class="item"><div class="label"><i class="news icon"></i></div><div class="content"><div class="header"><a href="{url}">{title}</a></div><div class="meta"><span class="date">{date}</span><span class="user">{author}</span></div><div class="description">{body}</div></div></div>',
+        entryTemplate: '<div class="item"><div class="content"><div class="header"><a href="{url}">{title}</a></div><div class="meta"><span class="date">{date}</span><span class="user">{author}</span></div><div class="description">{body}</div></div></div>',
         dateFormat: 'MMMM Do, YYYY',
 
     },
